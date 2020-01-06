97787a4815cea56fb73056c08fc5353b04ac2fa1
Tintou <martin.lm@outlook.fr>
2020-01-06 16:22:44 +0100

    Ajout de 2 boutons test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Pomodedouche
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
