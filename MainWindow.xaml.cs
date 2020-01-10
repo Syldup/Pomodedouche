50468359f37ff888011f6cb6a59591b74dad1e2c
Tintou <martin.lm@outlook.fr>
2020-01-10 11:40:04 +0100

    Test ajout grid via code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using System.Collections.ObjectModel;
+=======
+using System.IO;
+>>>>>>> Stashed changes
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +13,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -38,9 +43,45 @@ namespace Pomodedouche
             this.tags = new List<Tag>();
             this.DataContext = new ObservableCollection<Tag>(this.tags);
 
+            var window = new Window();
+
             update_lbTimer();
             timer.Interval = TimeSpan.FromMilliseconds(5);
             timer.Tick += timer_Tick;
+
+
+            /// Create a grid with some text within
+            // Grid
+            Grid blocPomo1 = new Grid();
+            blocPomo1.Width = 201;
+            blocPomo1.Height = 87;
+            blocPomo1.HorizontalAlignment = HorizontalAlignment.Left;
+            blocPomo1.VerticalAlignment = VerticalAlignment.Top;
+
+            // Rows
+            RowDefinition gridRow1 = new RowDefinition();
+            gridRow1.Height = new GridLength(45);
+            RowDefinition gridRow2 = new RowDefinition();
+            gridRow2.Height = new GridLength(45);
+            RowDefinition gridRow3 = new RowDefinition();
+            gridRow3.Height = new GridLength(45);
+            blocPomo1.RowDefinitions.Add(gridRow1);
+            blocPomo1.RowDefinitions.Add(gridRow2);
+            blocPomo1.RowDefinitions.Add(gridRow3);
+
+            // Text
+            Label labelPomodoroName1 = new Label();
+            labelPomodoroName1.Content = "Pomodoro test 1";
+            labelPomodoroName1.FontSize = 18;
+            labelPomodoroName1.Foreground = new SolidColorBrush(Colors.White);
+            labelPomodoroName1.VerticalAlignment = VerticalAlignment.Top;
+
+            Grid.SetRow(labelPomodoroName1, 0);
+
+            blocPomo1.Children.Add(labelPomodoroName1);
+
+            window.Content = blocPomo1;
+
         }
 
         void timer_Tick(object sender, EventArgs e)
